2087909a4a521d116c9d119e81dd009d2d559dd5
Daniel Franklin <daniel@danielzfranklin.org>
2026-05-08 22:47:29 +0100

    minor

--- src/logger.ts
+++ src/logger.ts
@@ -2,4 +2,11 @@ import pino from "pino";
 
 export const logger = pino({
   level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  base: undefined,
+  timestamp: pino.stdTimeFunctions.isoTime,
+  formatters: {
+    level(label) {
+      return { level: label };
+    },
+  },
 });
